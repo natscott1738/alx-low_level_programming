b26812f567a62f7e7d0f2f6f043747a33cb09b8b
natscott1738 <natebaraka777@gmail.com>
2023-09-08 04:18:20 -0700

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
-*puts.c
-*  
-*
-*main - printing using the puts function
-*Return 0:
+ * main -print a line of code using puts
+ *
+ * Return: 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }    
