b1f07fad11e31c43699898ccaafd4e1e4eb51c6e
natscott1738 <natebaraka777@gmail.com>
2023-09-13 08:15:47 -0700

    Add 0x01-variables_if_else_while directory

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    /* Check if n is positive, zero, or negative */
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+
+    return (0);
+}
