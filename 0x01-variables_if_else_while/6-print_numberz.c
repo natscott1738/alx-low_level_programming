4d30aeedabfee078d46539dfae4111bd20317581
natscott1738 <natebaraka777@gmail.com>
2023-09-14 15:41:29 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	for (i = 0; i < 10; i++){
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
