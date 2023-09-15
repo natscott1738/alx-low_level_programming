ed77149d2e39050a2e389d83ffada54ef880282d
natscott1738 <natebaraka777@gmail.com>
2023-09-15 03:43:11 -0700

    sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - does something
+ * @n: is the variabl
+ *
+ * Return: returns 1 
+ * returns 0 and prints 0 if n == zero
+ * returns -1 and prints - if n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
