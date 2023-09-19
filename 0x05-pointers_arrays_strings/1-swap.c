489f74f96cdd503fe4c9409323297378dca29631
natscott1738 <natebaraka777@gmail.com>
2023-09-19 07:10:35 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - a function that swaps the values of two integers
+ * swap_int - a function that swaps the values of two integers
  * @a: parameter 1
  * @b: parameter 2
  */
