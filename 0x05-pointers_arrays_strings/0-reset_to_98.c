1c33fd21f64e8f3f35bf71cc39e52dc0dd89bdee
natscott1738 <natebaraka777@gmail.com>
2023-09-19 07:05:38 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 -  a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - a function that updates a pointer
  * @n: is the int to check for
  */
 
