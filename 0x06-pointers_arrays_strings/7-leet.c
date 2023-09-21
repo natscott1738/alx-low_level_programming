ab6af4fa9d6a36653e147f393fbc5032700bc9d1
natscott1738 <natebaraka777@gmail.com>
2023-09-20 23:48:11 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *leet - changes "aA" "eE" "oO" "tT" "lL" to 4 3 0 7 1
+ *
+ *@s: the pointer 
+ *
+ *Return: the string already converted
+ */
+char *leet(char *s)
+{
+	char l[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+	int i = 0;
+	int c;
+
+	while (s[i] != 0)
+	{
+		c = 0;
+		while (l[c] != 0)
+		{
+			if (s[i] == l[c])
+			{
+				s[i] = n[c];
+			}
+			c++;
+		}
+		i++;
+	}
+	return (s);
+}
