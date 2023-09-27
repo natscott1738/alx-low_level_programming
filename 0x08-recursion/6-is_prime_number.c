172762f4fc4b4954651a9b7fe2e16064bdd83dd0
natscott1738 <natebaraka777@gmail.com>
2023-09-27 07:55:27 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - checks if a number is a prime number
+ *
+ * @n: the number to be checked
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - helper function to check if a number is prime recursivelly
+ *
+ *@n: the number to be checked
+ *@divisor - the current divisor to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int check_prime(int n, int divisor)
+{
+	if (divisor <= n / 2)
+	{
+		if (n % divisor == 0)
+			return (0);
+		return (check_prime(n, divisor + 1));
+	}
+	return (1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int find_sq(int x, int y);
 int is_prime_number(int n);
+int check_prime(int n, int divisor);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 #endif
